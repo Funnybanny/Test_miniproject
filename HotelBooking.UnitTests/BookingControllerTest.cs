84743ad3d9cd8070c8b108761193a9cb9a01c305
Funnybanny <vazzzeg001@gmail.com>
2018-10-06 17:18:24 +0200

    add booking controller test

---
+++ HotelBooking.UnitTests/BookingControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using HotelBooking.Controllers;
+using HotelBooking.Models;
+using HotelBooking.UnitTests.Fakes;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace HotelBooking.UnitTests
+{
+    class BookingControllerTest
+    {
+        private BookingController controller;
+        private Mock<IRepository<Booking>> fakeBookingRepository;
+
+        public BookingControllerTest()
+        { 
+            fakeBookingRepository = new Mock<IRepository<Booking>>();
+
+            // Implement fake GetAll() method.
+            fakeBookingRepository.Setup(x => x.GetAll()).Returns(rooms);
+
+
+            // Implement fake Get() method.
+            //fakeRoomRepository.Setup(x => x.Get(2)).Returns(rooms[1]);
+
+
+            // Alternative setup with argument matchers:
+
+            // Any integer:
+            //roomRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(rooms[1]);
+
+            // Integers from 1 to 2 (using a predicate)
+            // If the fake Get is called with an another argument value than 1 or 2,
+            // it returns null, which corresponds to the behavior of the real
+            // repository's Get method.
+            //fakeBookingRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(|rooms[1]|);
+
+            // Integers from 1 to 2 (using a range)
+            //roomRepository.Setup(x => x.Get(It.IsInRange<int>(1, 2, Range.Inclusive))).Returns(rooms[1]);
+
+
+            // Create BookingController
+            controller = new BookingController(fakeBookingRepository.Object);
+        }
+
+    }
+}
