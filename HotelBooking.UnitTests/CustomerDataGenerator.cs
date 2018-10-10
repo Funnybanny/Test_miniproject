5c9d1678aa69f82408e15134b1cb837f933e59df
Mareks Kalējs <M.Kalejs@outlook.com>
2018-10-10 19:24:58 +0200

    Customer Data Generator

---
+++ HotelBooking.UnitTests/CustomerDataGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using HotelBooking.Models;
+
+namespace HotelBooking.UnitTests
+{
+    public class CustomerDataGenerator : IEnumerable<Customer>
+    {
+
+        private readonly List<Customer> _customers = new List<Customer>
+        {
+            new Customer { Id=1, Email = "joe@outlook.com", Name = "Joe"},
+            new Customer { Id=2, Email = "bill@outlook.com", Name = "Billy" },
+        };
+
+
+        public IEnumerator<Customer> GetEnumerator()
+        {
+            return _customers.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
