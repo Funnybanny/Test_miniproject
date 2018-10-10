346b534b5840f02ce01e0dcf7b2e9a56b4c84ae9
Funnybanny <vazzzeg001@gmail.com>
2018-10-10 20:01:10 +0200

    hotelbooking not done

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using HotelBooking.BusinessLogic;
 using HotelBooking.Models;
 using HotelBooking.UnitTests.Fakes;
@@ -14,6 +15,23 @@ namespace HotelBooking.UnitTests
         private Mock<IRepository<Room>> fakeRoomRepository;
 
         public BookingManagerTests(){
+
+            var rooms = new List<Room>
+            {
+                new Room { Id=1, Description="A" },
+                new Room { Id=2, Description="B" },
+            };
+            var customers = new List<Customer>
+            {
+                new Customer { Id=1, Email = "joe@outlook.com", Name = "Joe"},
+                new Customer { Id=2, Email = "bill@outlook.com", Name = "Billy" },
+            };
+            List<Booking> bookings = new List<Booking>
+            {
+                new Booking {Id=1, StartDate=DateTime.Now, EndDate=DateTime.Now, IsActive=true, CustomerId=1, RoomId=1, Customer=customers[0], Room=rooms[0]},
+                new Booking {Id=2, StartDate=DateTime.Now, EndDate=DateTime.Now, IsActive=true, CustomerId=2, RoomId=2, Customer=customers[1], Room=rooms[1]}
+            };
+
             DateTime start = DateTime.Today.AddDays(10);
             DateTime end = DateTime.Today.AddDays(20);
             fakeBookingRepository.Setup(x => x.GetAll()).Returns(bookings);
@@ -23,6 +41,19 @@ namespace HotelBooking.UnitTests
             fakeRoomRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(rooms[1]);
 
             bookingManager = new BookingManager(fakeBookingRepository.Object, fakeRoomRepository.Object);
+        }
+
+        public IEnumerable<object[]> GetBookings()
+        {
+            yield return new object[] { 1, DateTime.Now, DateTime.Now, true, 1, 1, customers[0], rooms[0] };
+            yield return new object[] { 2, DateTime.Now, DateTime.Now, true, 2, 2, customers[1], rooms[1] }
+        }
+
+        [Theory]
+        []
+        public void CreateBookingTest()
+        {
+            bookingManager
         }
 
         [Fact]
