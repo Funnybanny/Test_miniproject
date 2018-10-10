d7370d1def16d75b73bf5eaf8d2276db7b69cfa2
Funnybanny <vazzzeg001@gmail.com>
2018-10-10 19:31:51 +0200

    Roomdatagenerator

---
+++ HotelBooking.UnitTests/RoomDataGenerator.cs
@@ -0,0 +1,28 @@
+﻿using HotelBooking.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBooking.UnitTests
+{
+    public class RoomDataGenerator: IEnumerable<Room>
+    {
+        private readonly List<Room> _rooms = new List<Room>
+        {
+            new Room { Id=1, Description="A" },
+            new Room { Id=2, Description="B" },
+        };
+
+
+        public IEnumerator<Room> GetEnumerator()
+        {
+            return _rooms.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
