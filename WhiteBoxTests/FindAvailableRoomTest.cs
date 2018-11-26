adc0e80513033e1ac32d3ae52089a5300ca15f94
Funnybanny <vazzzeg001@gmail.com>
2018-11-26 14:22:04 +0100

    more setup

---
+++ WhiteBoxTests/FindAvailableRoomTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Moq;
+using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhiteBoxTests
+{
+    public class FindAvailableRoomTest
+    {
+        /*edge
+         */
+        [Fact]
+        public void Edge1()
+        {
+        }
+
+    }
+}
