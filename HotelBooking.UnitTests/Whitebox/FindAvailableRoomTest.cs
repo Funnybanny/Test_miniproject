3cd76250aa0bd94cce698a33d962d7781ba539ea
Funnybanny <vazzzeg001@gmail.com>
2018-11-27 13:19:26 +0100

    FindAvailableRoom Loop Coverage

--- HotelBooking.UnitTests/Whitebox/FindAvailableRoomTest.cs
+++ HotelBooking.UnitTests/Whitebox/FindAvailableRoomTest.cs
@@ -19,8 +19,15 @@ namespace WhiteBoxTests
         private Mock<IRepository<Customer>> fakeCustomerRepository;
         private Mock<IRepository<Room>> fakeRoomRepository;
         private BookingManager bookingmanager;
+        private BookingManager loopmanager;
         private BookingViewModel bookingmodel;
 
+        private BookingsController controllerLoop;
+        private BookingManager bookingmanagerLoop;
+        private BookingManager loopmanagerLoop;
+        private BookingViewModel bookingmodelLoop;
+        private Mock<IRepository<Booking>> fakeBookingRepositoryLoop;
+        private Mock<IRepository<Room>> fakeRoomRepositoryLoop;
 
         public FindAvailableRoomTest()
         {
@@ -28,11 +35,15 @@ namespace WhiteBoxTests
             fakeCustomerRepository = new Mock<IRepository<Customer>>();
             fakeRoomRepository = new Mock<IRepository<Room>>();
 
+            fakeRoomRepositoryLoop = new Mock<IRepository<Room>>();
+            fakeBookingRepositoryLoop = new Mock<IRepository<Booking>>();
+
             var rooms = new List<Room>
             {
                 new Room {Id = 1, Description = "A"},
                 new Room {Id = 2, Description = "B"},
             };
+
             var customers = new List<Customer>
             {
                 new Customer {Id = 1, Email = "joe@outlook.com", Name = "Joe"},
@@ -52,12 +63,22 @@ namespace WhiteBoxTests
                 }
             };
 
+            var roomsLoop = new List<Room>
+            {
+
+            };
+            List<Booking> bookingsLoop = new List<Booking>
+            {
+
+            };
 
             // Implement fake GetAll() method.
             fakeBookingRepository.Setup(x => x.GetAll()).Returns(bookings);
             fakeCustomerRepository.Setup(x => x.GetAll()).Returns(customers);
             fakeRoomRepository.Setup(x => x.GetAll()).Returns(rooms);
 
+            fakeRoomRepositoryLoop.Setup(x => x.GetAll()).Returns(roomsLoop);
+            fakeBookingRepositoryLoop.Setup(x => x.GetAll()).Returns(bookingsLoop);
             // Implement fake Get() method.
             //fakeBookingRepository.Setup(x => x.Get(2)).Returns(bookings[1]);
 
@@ -74,6 +95,7 @@ namespace WhiteBoxTests
             fakeBookingRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(bookings[1]);
             fakeCustomerRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(customers[1]);
             fakeRoomRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(rooms[1]);
+            
 
             // Integers from 1 to 2 (using a range)
             //roomRepository.Setup(x => x.Get(It.IsInRange<int>(1, 2, Range.Inclusive))).Returns(rooms[1]);
@@ -83,13 +105,23 @@ namespace WhiteBoxTests
             bookingmodel = new BookingViewModel(fakeBookingRepository.Object, bookingmanager);
             controller = new BookingsController(fakeBookingRepository.Object, fakeRoomRepository.Object,
                 fakeCustomerRepository.Object, bookingmanager, bookingmodel);
+
+            bookingmanagerLoop = new BookingManager(fakeBookingRepositoryLoop.Object, fakeRoomRepositoryLoop.Object);
+            bookingmodelLoop = new BookingViewModel(fakeBookingRepositoryLoop.Object, bookingmanagerLoop);
+            controllerLoop = new BookingsController(fakeBookingRepositoryLoop.Object, fakeRoomRepositoryLoop.Object,
+                fakeCustomerRepository.Object, bookingmanagerLoop, bookingmodelLoop);
         }
 
-        /*edge coverage 
+        /*  Edge coverage 
          * On diagram Node 1
          * if then
          * node 3
          * node 7
+         * node 5
+         *  
+         *  Loop Coverage
+         * node 4
+         * iteration = 2
          */
         [Fact]
         public void Edge1()
@@ -110,15 +142,25 @@ namespace WhiteBoxTests
         }
 
         /*edge coverage 
-         * On diagram Node 4
+         * On diagram Node 6
          * if else
          */
         [Fact]
-        public void Edge4()
+        public void Edge6()
         {
             var returnValue = bookingmanager.FindAvailableRoom(DateTime.Today.AddDays(2), DateTime.Today.AddDays(3));
             Assert.Equal(-1, returnValue);
         }
 
+        /*  Loop Coverage
+         * node 4
+         * iteration = 0
+         */
+        [Fact]
+        public void Loop0()
+        {
+            var returnValue = bookingmanagerLoop.FindAvailableRoom(DateTime.Today.AddDays(2), DateTime.Today.AddDays(3));
+            Assert.Equal(-1, returnValue);
+        }
     }
 }
