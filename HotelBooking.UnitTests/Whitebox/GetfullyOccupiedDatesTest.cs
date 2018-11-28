ea1ad1f149083339d9bd6dcbe87f9eb5cbd45efa
Bence Szilveszter Hajdu <benc0054@easv365.dk>
2018-11-28 13:25:37 +0100

    Condition coverage for GetFullyOccupiedDates

--- HotelBooking.UnitTests/Whitebox/GetfullyOccupiedDatesTest.cs
+++ HotelBooking.UnitTests/Whitebox/GetfullyOccupiedDatesTest.cs
@@ -25,9 +25,15 @@ namespace HotelBooking.UnitTests.Whitebox
         private BookingViewModel Emptybookingmodel;
         private BookingsController Emptycontroller;
 
+        private Mock<IRepository<Booking>> fake2BookingRepository;
+        private BookingManager booking2manager;
+        private BookingViewModel booking2model;
+        private BookingsController controller2;
+
         public GetfullyOccupiedDatesTest()
         {
             fakeBookingRepository = new Mock<IRepository<Booking>>();
+            fake2BookingRepository = new Mock<IRepository<Booking>>();
             fakeCustomerRepository = new Mock<IRepository<Customer>>();
             fakeRoomRepository = new Mock<IRepository<Room>>();
 
@@ -57,6 +63,24 @@ namespace HotelBooking.UnitTests.Whitebox
                     RoomId = 2, Customer = customers[1], Room = rooms[1]
                 }
             };
+            List<Booking> bookingstwo = new List<Booking>
+            {
+                new Booking
+                {
+                    Id = 1, StartDate = DateTime.Today.AddDays(2), EndDate = DateTime.Today.AddDays(3), IsActive = true, CustomerId = 1,
+                    RoomId = 1, Customer = customers[0], Room = rooms[0]
+                },
+                new Booking
+                {
+                    Id = 2, StartDate = DateTime.Today.AddDays(1), EndDate = DateTime.Today.AddDays(5), IsActive = true, CustomerId = 2,
+                    RoomId = 2, Customer = customers[1], Room = rooms[1]
+                },
+                new Booking
+                {
+                    Id = 2, StartDate = DateTime.Today.AddDays(1), EndDate = DateTime.Today.AddDays(4), IsActive = false, CustomerId = 2,
+                    RoomId = 2, Customer = customers[1], Room = rooms[1]
+                }
+            };
 
             List<Booking> Emptybookings = new List<Booking>
             {
@@ -69,6 +93,7 @@ namespace HotelBooking.UnitTests.Whitebox
             fakeRoomRepository.Setup(x => x.GetAll()).Returns(rooms);
 
             EmptyfakeBookingRepository.Setup(x => x.GetAll()).Returns(Emptybookings);
+            fake2BookingRepository.Setup(x => x.GetAll()).Returns(bookingstwo);
 
             // Implement fake Get() method.
             //fakeBookingRepository.Setup(x => x.Get(2)).Returns(bookings[1]);
@@ -101,6 +126,11 @@ namespace HotelBooking.UnitTests.Whitebox
             Emptybookingmodel = new BookingViewModel(EmptyfakeBookingRepository.Object, Emptybookingmanager);
             Emptycontroller = new BookingsController(EmptyfakeBookingRepository.Object, fakeRoomRepository.Object,
                 fakeCustomerRepository.Object, Emptybookingmanager, Emptybookingmodel);
+
+            booking2manager = new BookingManager(fake2BookingRepository.Object, fakeRoomRepository.Object);
+            booking2model = new BookingViewModel(fake2BookingRepository.Object, booking2manager);
+            controller2 = new BookingsController(fake2BookingRepository.Object, fakeRoomRepository.Object,
+                fakeCustomerRepository.Object, booking2manager, booking2model);
         }
 
         /*  Edge coverage 
@@ -165,6 +195,32 @@ namespace HotelBooking.UnitTests.Whitebox
             var returnValue = bookingmanager.GetFullyOccupiedDates(DateTime.Today, DateTime.Today);
             Assert.Empty(returnValue);
          }
-        
+
+        /* Condition Coverage
+         * T T T
+         * T=True
+         * F=False
+         * N=Doesn't matter
+         * Booking is active
+         * Start date is after or at given start date
+         * End date is before or at given end date
+         * Expected result: Two datetimes
+         * */
+        /* Condition Coverage
+        * F N N
+        * T=True
+        * F=False
+        * N=Doesn't matter
+        * Booking is inactive
+        * Expected result: Two datetimes
+        * */
+        [Fact]
+        public void ConditionTTT()
+        {
+            DateTime startdate = DateTime.Today;
+            DateTime enddate = DateTime.Today.AddDays(10);
+            List<DateTime> fullyOccupiedDates = booking2manager.GetFullyOccupiedDates(startdate, enddate);
+            Assert.Equal(2, fullyOccupiedDates.Count);
+        }
     }
 } 
