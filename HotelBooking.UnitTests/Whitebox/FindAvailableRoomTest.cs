bea133a46f6dce9aaf24bc973700adf5a92982fe
Mareks Kalējs <M.Kalejs@outlook.com>
2018-11-28 18:57:02 +0100

    Minor code cleanup

--- HotelBooking.UnitTests/Whitebox/FindAvailableRoomTest.cs
+++ HotelBooking.UnitTests/Whitebox/FindAvailableRoomTest.cs
@@ -92,26 +92,12 @@ namespace HotelBooking.UnitTests.Whitebox
 
             fakeRoomRepositoryLoop.Setup(x => x.GetAll()).Returns(roomsLoop);
             fakeBookingRepositoryLoop.Setup(x => x.GetAll()).Returns(bookingsLoop);
+
             // Implement fake Get() method.
-            //fakeBookingRepository.Setup(x => x.Get(2)).Returns(bookings[1]);
-
-
-            // Alternative setup with argument matchers:
-
-            // Any integer:
-            //roomRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(rooms[1]);
-
-            // Integers from 1 to 2 (using a predicate)
-            // If the fake Get is called with an another argument value than 1 or 2,
-            // it returns null, which corresponds to the behavior of the real
-            // repository's Get method.
             fakeBookingRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(bookings[1]);
             fakeCustomerRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(customers[1]);
             fakeRoomRepository.Setup(x => x.Get(It.Is<int>(id => id > 0 && id < 3))).Returns(rooms[1]);
 
-
-            // Integers from 1 to 2 (using a range)
-            //roomRepository.Setup(x => x.Get(It.IsInRange<int>(1, 2, Range.Inclusive))).Returns(rooms[1]);
 
             // Create BookingController
             bookingmanager = new BookingManager(fakeBookingRepository.Object, fakeRoomRepository.Object);
